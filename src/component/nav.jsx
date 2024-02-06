e981acb4336f15042792c83e60fcf5a1ea12c811
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-02-06 22:47:05 +0330

    design by mobile is complated

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -4,39 +4,42 @@ import logo from '../asset/logo1.11c7f5e0.webp'
 import './style.css'
 
 const Nav = () => {
-  return ( 
-  <>
-    <div className="navbar navbar-expand-lg w-100" >
-      <div className="row container headernav h-50">
-        <div className="col-6 ">
-          <a href='www.instagram/' className='nav-icon ' id='icnonav'>
-            <Icon.Instagram id='icnonav'  size={24} />
-          </a>
-          <a href='www.telegram/' className='nav-icon' id='icnonav'>
-            <Icon.Telegram  id='icnonav' size={24} />
-          </a>
-        </div>
-        <div className="col-6">
-          <a href='/'>
-            <img className='logo' src={logo} alt="" />
-          </a>
-        </div>
-          
-      </div>
-    
+  return (
+    <>
+      <div className=" nav-top
+    ">
+        <div className="      
+        sticky w-100 border-black0 mx-0 mt-0 bg-white
+        md:hidden
+      ">
+          <div className="
+      w-90 mx-auto flex
+      ">
+            <div className="w-1/3 flex-initial mt-3">
+              <a className='text-red-300 flex-initial mr-4' href="/"><Icon.Instagram className='h-auto w-7 ' /></a>
+              <a className='text-red-300 flex-initial ' href="/"><Icon.Telegram className='h-auto w-7' /></a>
+            </div>
+            <div className="w-1/3"></div>
+            <div className="w-1/3 flex-initial">
+              <a href="/">
+                <img className='h-auto w-28 mt-1' src={logo} alt="" />
+              </a>
+            </div>
 
-      <div className="row h-50 d-flex w-100">
-        <div className="w-75 col-lg-10">
-            <input className=' m-4 w-75' type="text" placeholder='search...' />
+          </div>
+          <div className="
+        w-90 flex mt-auto
+        ">
+            <input className='w-80 h-12 rounded-xl mx-3 my-1' type="text" placeholder='search product ...' />
+          </div>
         </div>
-        <div className="w-lg-100">
-          
+
+        <div className="max-md:hidden bg-red-200 w-100 h-50">
+          hi
         </div>
+
       </div>
-      
-    </div>
-    
-  </> );
+    </>);
 }
- 
+
 export default Nav;
