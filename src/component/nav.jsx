6e2a06b4e7dc72896ab006b48debbff8ff08d6ed
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-02-11 16:32:15 +0330

    mobile design commit

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,5 +1,5 @@
 import * as Icon from 'react-bootstrap-icons';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../asset/logo1.11c7f5e0.webp'
 import './style.css'
 
@@ -21,21 +21,22 @@ const Nav = () => {
             </div>
             <div className="w-1/3"></div>
             <div className="w-1/3 flex-initial">
-              <a href="/">
-                <img className='h-auto w-28 mt-1' src={logo} alt="" />
-              </a>
+              <NavLink to='/'>
+                <img className='h-auto w-28 mt-1' src={logo} alt="logo" />
+              </NavLink>
             </div>
 
           </div>
           <div className="
-        w-90 flex mt-auto
-        ">
-            <input className='w-80 h-12 rounded-xl mx-3 my-1' type="text" placeholder='search product ...' />
+            w-90 flex mt-auto justify-end
+            ">
+            <input className='w-[80%] h-12 rounded-xl  mr-auto ml-[15px] my-1' type="text" placeholder='search product ...' />
+            <button className='w-[41px] h-[41px] mt-[4px] mx-[15px]  text-center items-center rounded-lg bg-red-300 bg-opacity-80 border-none text-3xl font-extrabold flex content-center'><Icon.List className='text-white ' size={32} /></button>
           </div>
         </div>
 
         <div className="max-md:hidden bg-red-200 w-100 h-50">
-          hi
+          h
         </div>
 
       </div>
